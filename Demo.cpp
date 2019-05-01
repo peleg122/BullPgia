3f2bf63d916ad9eaf02b1a7fe36ea2294c43abed
peleg122 <peleg122@gmail.com>
2019-05-01 12:41:28 +0300

    Update Demo.cpp

--- Demo.cpp
+++ Demo.cpp
@@ -19,7 +19,7 @@ using namespace std;
 #include "SmartGuesser.hpp"
 using namespace bullpgia;
 
-/*int main() {
+int main() {
     ConstantChooser c1234{"1234"}, c12345{"12345"}, c9999{"9999"};
     ConstantGuesser g1234{"1234"}, g12345{"12345"}, g9999{"9999"};
     cout << play(c1234, g1234, 4, 100) << endl;  // prints 1 - guesser wins in one turn.
@@ -38,5 +38,5 @@ using namespace bullpgia;
         cout << play(randy, smarty, 4, 100) << endl;  // smarty should always win in at most 10 turns!
     }
     return 0;
-}*/
+}
 
